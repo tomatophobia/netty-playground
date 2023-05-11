0a7a36ccebd8fdf922137f15ac969fbb40a92db0
Youngseo Choi <ys550499@gmail.com>
2023-05-12 01:04:28 +0900

    데이터 쪼개져서 올 때 first solution

--- src/main/java/example/time/TimeClientHandler.java
+++ src/main/java/example/time/TimeClientHandler.java
@@ -7,15 +7,31 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 import java.util.Date;
 
 public class TimeClientHandler extends ChannelInboundHandlerAdapter {
+    private ByteBuf buf;
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        buf = ctx.alloc().buffer(4);
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        buf.release();
+        buf = null;
+    }
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf m = (ByteBuf) msg;
-        try {
+        buf.writeBytes(m);
+        m.release();
+
+        // 4byte를 모두 받았을 때만
+        // 쪼개져서 올 때는 channelRead가 한 번 더 호출되면서 buf의 남은 부분을 채울 것이다.
+        if (buf.readableBytes() >= 4) {
             long currentTimeMillis = (m.readUnsignedInt() - 2208988800L) * 1000L;
             System.out.println(new Date(currentTimeMillis));
             ctx.close();
-        } finally {
-            m.release();
         }
     }
 
