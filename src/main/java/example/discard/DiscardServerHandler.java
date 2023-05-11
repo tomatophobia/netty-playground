1bc5aa1a7735137e468f3c6bed9d0d176bdbab24
Youngseo Choi <ys550499@gmail.com>
2023-05-11 18:59:50 +0900

    DISCARD 서버

--- src/main/java/example/discard/DiscardServerHandler.java
+++ src/main/java/example/discard/DiscardServerHandler.java
@@ -3,6 +3,7 @@ package example.discard;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
 
 /**
  * Handles a server-side channel.
@@ -10,8 +11,18 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        // Discard the received data silently.
-        ((ByteBuf) msg).release();
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            while (in.isReadable()) {
+                System.out.println((char) in.readByte());
+                System.out.flush();
+            }
+
+            // this loop can be simplified to ...
+            // System.out.println(in.toString(io.netty.util.CharsetUtil.US_ASCII));
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
     }
 
     @Override
